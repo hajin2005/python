3e8009fb0763f8c79433b3f004ddc5dcb241b6ae
jinaoah <w2128@e-mirim.hs.kr>
2022-12-08 10:00:42 +0900

    classMethod 공부 : cls, @classmethod

--- class/classmethodTest.py
+++ class/classmethodTest.py
@@ -6,9 +6,9 @@ class Car:
         self.speed = speed
         Car.count += 1
 
-    # @classmethod
-    def get_count(self):
-        return Car.count
+    @classmethod
+    def get_count(cls): #self 매개변수 필요하지 않음, 
+        return Car.count #count 값을 넣을 객체 생성을 하지 않아도 됨
 
 # @classmethod - cls 를 사용했을 경우 객체 생성을 먼저 하지 않아도 됨.
 # print(Car.get_count()) -> 매개변수 self로 작성했을 경우 에러, 객체 생성 후 사용해야함
